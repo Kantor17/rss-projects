edff66f57cb0f5e6a837f98cf3582df5c14a8efb
Maksym Kantor <max.kantor72@gmail.com>
2022-07-03 20:12:52 +0300

    feat: implement controller migration

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '83c309dc7b1746e48a4426f990325996',
         });
     }
 }
--- migration-to-ts/src/components/controller/loader.ts
+++ migration-to-ts/src/components/controller/loader.ts
@@ -1,19 +1,34 @@
+type Options = Partial<{
+    [index: string]: string;
+}>;
+type InitOptions = Options & { apiKey: string };
+type Endpoints = 'everything' | 'top-headlines' | 'sources';
+export type CallBack = (data: Data) => void;
+export interface Data {
+    articles: object[];
+    status: string;
+    totalResults: number;
+}
+
 class Loader {
-    constructor(baseLink, options) {
+    baseLink: string;
+    options: Options;
+    constructor(baseLink: string, options: InitOptions) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
-        callback = () => {
+        { endpoint, options }: { endpoint: Endpoints; options?: Options },
+        callback: CallBack = (data: Data) => {
+            console.log(data);
             console.error('No callback for GET response');
         }
     ) {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response) {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,7 +38,7 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
+    makeUrl(endpoint: Endpoints, options?: Options) {
         const urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
@@ -34,11 +49,11 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
-        fetch(this.makeUrl(options, endpoint), { method })
+    load(method: 'GET' | 'POST', endpoint: Endpoints, callback: CallBack, options?: Options) {
+        fetch(this.makeUrl(endpoint, options), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
-            .then((data) => callback(data))
+            .then((data: Data) => callback(data))
             .catch((err) => console.error(err));
     }
 }
