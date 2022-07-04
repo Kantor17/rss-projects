f503d9ff1aaf32114f54016447293d8028e7c971
Maksym Kantor <max.kantor72@gmail.com>
2022-07-04 10:59:57 +0300

    fix: change API key due to 429 error

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '83c309dc7b1746e48a4426f990325996',
+            apiKey: '94548ad170e84e558cc871a48aabe065',
         });
     }
 }
