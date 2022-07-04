b665147f0b2e57fdff471b4756e4ed3b3a6cfdc1
Maksym Kantor <max.kantor72@gmail.com>
2022-07-04 20:41:35 +0300

    feat: add news image placeholder

--- migration-to-ts/src/components/view/news/news.ts
+++ migration-to-ts/src/components/view/news/news.ts
@@ -14,7 +14,7 @@ class News implements NewsInterface {
             if (idx % 2) (newsClone.querySelector('.news__item') as HTMLElement).classList.add('alt');
 
             (newsClone.querySelector('.news__meta-photo') as HTMLElement).style.backgroundImage = `url(${
-                item.urlToImage || 'img/news_placeholder.jpg'
+                item.urlToImage || 'http://resistantvirus.com/wp-content/themes/resistant/images/news-placeholder.png'
             })`;
             (newsClone.querySelector('.news__meta-author') as HTMLElement).textContent =
                 item.author || item.source.name;
