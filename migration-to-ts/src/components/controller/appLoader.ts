c78cca97d0f16890f0ca70582d19ef26256d8dc4
Maksym Kantor <max.kantor72@gmail.com>
2022-07-04 23:34:31 +0300

    fix: prevent from possible deploy problems

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '94548ad170e84e558cc871a48aabe065',
         });
     }
